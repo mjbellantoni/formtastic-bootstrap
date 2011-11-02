c7b023e56495475749819d529c0bae1048955a7f
Matthew Bellantoni <mjbellantoni@yahoo.com>
2011-11-02 14:35:40 -0400

    Tidying up.

--- lib/formtastic-bootstrap/inputs/select_input.rb
+++ lib/formtastic-bootstrap/inputs/select_input.rb
@@ -2,8 +2,6 @@ module FormtasticBootstrap
   module Inputs
     class SelectInput < Formtastic::Inputs::SelectInput
       include Base
-      # include Base::Collections
-      # include Base::GroupedCollections
     end
   end
 end
