ff6058ce72906089d5706b36b2af7c1c70657e58
Matthew Bellantoni <mjbellantoni@yahoo.com>
2011-11-02 12:41:41 -0400

    Refactor.

--- lib/formtastic-bootstrap/form_builder.rb
+++ lib/formtastic-bootstrap/form_builder.rb
@@ -2,50 +2,57 @@ module FormtasticBootstrap
   
   class FormBuilder < Formtastic::FormBuilder
 
-    def text_area(method, input_html_options)
+    def bootstrap_input_wrapper
       template.content_tag(:div, :class => "input") do 
-        super(method, input_html_options)
+        yield
       end
     end
-
-    def text_field(method, input_html_options)
-      template.content_tag(:div, :class => "input") do 
-        super(method, input_html_options)
-      end
-    end
-
-    def password_field(method, input_html_options)
-      template.content_tag(:div, :class => "input") do 
-        super(method, input_html_options)
-      end
-    end
+    private :bootstrap_input_wrapper
 
     def email_field(method, input_html_options)
-      template.content_tag(:div, :class => "input") do 
+      bootstrap_input_wrapper do
         super(method, input_html_options)
       end
     end
 
     def hidden_field(method, input_html_options)
-      template.content_tag(:div, :class => "input") do 
+      bootstrap_input_wrapper do
+        super(method, input_html_options)
+      end
+    end
+
+    def password_field(method, input_html_options)
+      bootstrap_input_wrapper do
         super(method, input_html_options)
       end
     end
 
     def phone_field(method, input_html_options)
-      template.content_tag(:div, :class => "input") do 
+      bootstrap_input_wrapper do
         super(method, input_html_options)
       end
     end
 
     def search_field(method, input_html_options)
-      template.content_tag(:div, :class => "input") do 
+      bootstrap_input_wrapper do
+        super(method, input_html_options)
+      end
+    end
+
+    def text_area(method, input_html_options)
+      bootstrap_input_wrapper do
+        super(method, input_html_options)
+      end
+    end
+
+    def text_field(method, input_html_options)
+      bootstrap_input_wrapper do
         super(method, input_html_options)
       end
     end
 
     def url_field(method, input_html_options)
-      template.content_tag(:div, :class => "input") do 
+      bootstrap_input_wrapper do
         super(method, input_html_options)
       end
     end
