d68bbf6d2ae53863fa7090a869005498b7a67878
Matthew Bellantoni <mjbellantoni@yahoo.com>
2012-04-14 13:34:26 -0400

    Additional simplification of spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,11 @@
+require 'bundler/setup'
+
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 # require 'rspec'
 # require 'rspec_tag_matchers'
 # require 'rubygems'
-
-require 'bundler'
-Bundler.setup
 
 # Find Formtastic and grab its testing support first.
 formtastic_full_gem_path = Bundler.load.specs.find{|s| s.name == "formtastic" }.full_gem_path
