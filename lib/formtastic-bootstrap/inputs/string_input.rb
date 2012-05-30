fd1af1a495dfba00a08c9904fae7fcfe3168b23a
Matthew Bellantoni <mjbellantoni@yahoo.com>
2012-05-29 22:31:52 -0400

    Reimplement StringInput.

--- lib/formtastic-bootstrap/inputs/string_input.rb
+++ lib/formtastic-bootstrap/inputs/string_input.rb
@@ -4,11 +4,12 @@ module FormtasticBootstrap
       include Base
       include Base::Stringish
 
-      def to_html
-        generic_input_wrapping do
-          builder.text_field(method, input_html_options)
-        end
-      end
+      # def to_html
+      #   puts "StringInput#to_html"
+      #   generic_input_wrapping do
+      #     builder.text_field(method, input_html_options)
+      #   end
+      # end
 
     end
   end
