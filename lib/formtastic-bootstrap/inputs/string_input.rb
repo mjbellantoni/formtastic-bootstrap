2069c8fe3099a040cd59916ad657511bf9dfdb33
Matthew Bellantoni <mjbellantoni@yahoo.com>
2012-06-10 11:45:06 -0400

    Remove commented-out code.

--- lib/formtastic-bootstrap/inputs/string_input.rb
+++ lib/formtastic-bootstrap/inputs/string_input.rb
@@ -3,14 +3,6 @@ module FormtasticBootstrap
     class StringInput < Formtastic::Inputs::StringInput
       include Base
       include Base::Stringish
-
-      # def to_html
-      #   puts "StringInput#to_html"
-      #   generic_input_wrapping do
-      #     builder.text_field(method, input_html_options)
-      #   end
-      # end
-
     end
   end
 end
