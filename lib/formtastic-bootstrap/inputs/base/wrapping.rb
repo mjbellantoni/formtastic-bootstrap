5ca2bb621dbd9b476c386e4c7dfc6d798e57ebdb
Adam Saegebarth <adams@synapse.com>
2012-06-12 17:12:56 -0700

    remove carriage return in appended and prepended elements to avoid little gap

--- lib/formtastic-bootstrap/inputs/base/wrapping.rb
+++ lib/formtastic-bootstrap/inputs/base/wrapping.rb
@@ -11,15 +11,15 @@ module FormtasticBootstrap
             input_div_wrapping do
               if options[:prepend] && !options[:append]
                 prepended_input_wrapping do
-                  [template.content_tag(:span, options[:prepend], :class => 'add-on'), yield].join("\n").html_safe
+                  [template.content_tag(:span, options[:prepend], :class => 'add-on'), yield].join.html_safe
                 end
               elsif options[:append] && !options[:prepend]
                 appended_input_wrapping do
-                  [yield, template.content_tag(:span, options[:append], :class => 'add-on')].join("\n").html_safe
+                  [yield, template.content_tag(:span, options[:append], :class => 'add-on')].join.html_safe
                 end
               elsif options[:append] && options[:prepend]
                 appended_and_prepended_input_wrapping do
-                  [template.content_tag(:span, options[:prepend], :class => 'add-on'), yield, template.content_tag(:span, options[:append], :class => 'add-on')].join("\n").html_safe
+                  [template.content_tag(:span, options[:prepend], :class => 'add-on'), yield, template.content_tag(:span, options[:append], :class => 'add-on')].join.html_safe
                 end
               else
                 yield
