45252a759b8d5ff4eb875dfcc74fe020c3d22cdf
David Chapman <talk2dc@gmail.com>
2012-07-16 13:21:57 -0400

    Added country and date_select inputs

--- lib/formtastic-bootstrap/inputs/base/timeish.rb
+++ lib/formtastic-bootstrap/inputs/base/timeish.rb
@@ -5,21 +5,18 @@ module FormtasticBootstrap
 
         def date_input_html
           fragment_input_html(:date, "small")
+
         end
 
         def time_input_html
-          fragment_input_html(:time, "mini")
-        end
-
-        def fragment_id(fragment)
-          # TODO is this right?
-          # "#{input_html_options[:id]}_#{position(fragment)}i"
-          "#{input_html_options[:id]}[#{fragment}]"
+          default_date_fragments.map do |fragment|
+            fragment_input_html(:time, "mini")
+          end.join.html_safe
         end
 
         def fragment_input_html(fragment, klass)
           opts = input_options.merge(:prefix => fragment_prefix, :field_name => fragment_name(fragment), :default => value, :include_blank => include_blank?)
-          template.send(:"text_field_#{fragment}", value, opts, input_html_options.merge(:id => fragment_id(fragment), :class => klass))
+          template.send(:"select_#{fragment}", value, opts, input_html_options.merge(:id => fragment_id(fragment), :class => klass))
         end
 
       end
---
+++ lib/formtastic-bootstrap/inputs/country_input.rb
@@ -0,0 +1,12 @@
+module FormtasticBootstrap
+  module Inputs
+    class CountryInput < Formtastic::Inputs::CountryInput
+      include Base
+      def to_html
+        generic_input_wrapping do
+          builder.country_select(method, priority_countries, input_options, input_html_options)
+        end
+      end
+    end
+  end
+end
