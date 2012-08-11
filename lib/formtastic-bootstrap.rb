8ef88f1ee465cc5ac8060198229f59a3f7543eaa
Matthew Bellantoni <mjbellantoni@yahoo.com>
2012-08-11 07:54:59 -0400

    Autoload Inputs.

--- lib/formtastic-bootstrap.rb
+++ lib/formtastic-bootstrap.rb
@@ -1,7 +1,7 @@
 require "formtastic"
 require "formtastic-bootstrap/engine" if defined?(::Rails) # For tests
 require "formtastic-bootstrap/helpers"
-require "formtastic-bootstrap/inputs"
+# require "formtastic-bootstrap/inputs"
 require "formtastic-bootstrap/form_builder"
 require "action_view/helpers/text_field_date_helper"
 
@@ -13,7 +13,7 @@ module FormtasticBootstrap
   # autoload :Helpers
   # autoload :HtmlAttributes
   # autoload :I18n
-  # autoload :Inputs
+  autoload :Inputs,  "formtastic-bootstrap/inputs"
   autoload :Actions, "formtastic-bootstrap/actions"
   # autoload :LocalizedString
   # autoload :Localizer
