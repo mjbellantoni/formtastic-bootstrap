d20266883019fdcb6445b655f3c297628412c666
Matthew Bellantoni <mjbellantoni@yahoo.com>
2012-09-02 16:39:54 -0400

    Deprecate DatetimeInput.

--- lib/formtastic-bootstrap/inputs/datetime_input.rb
+++ lib/formtastic-bootstrap/inputs/datetime_input.rb
@@ -1,10 +1,7 @@
 module FormtasticBootstrap
   module Inputs
     class DatetimeInput < Formtastic::Inputs::DatetimeInput
-      def to_html
-        ::ActiveSupport::Deprecation.warn("DatetimeInput (:as => :datetime) has been renamed to DatetimeSelectInput (:as => :datetime_select) and will be removed or changed in the next version of Formtastic, please update your forms.", caller(2))
-        super
-      end
+      # This has been deprecated.
     end
   end
 end
