3b797ed4ef9755133975f3c66101d88549020791
Matthew Bellantoni <mjbellantoni@yahoo.com>
2012-09-03 17:41:09 -0400

    Implement SelectInput.

--- spec/inputs/select_input_spec.rb
+++ spec/inputs/select_input_spec.rb
@@ -143,6 +143,7 @@ describe 'select input' do
       end)
     end
 
+    it_should_have_bootstrap_horizontal_wrapping
     it_should_have_input_wrapper_with_class("select")
     it_should_have_input_wrapper_with_class(:input)
     it_should_have_input_wrapper_with_id("post_author_input")
