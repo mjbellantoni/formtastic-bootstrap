dd7d6cb4494522f67fb10e1b25a49d3f634b412d
Andrew Narkewicz <anark6@gmail.com>
2013-09-07 11:04:39 -0600

    Change close in alert from button to link so pressing enter in form still submits the form

--- lib/formtastic-bootstrap/helpers/errors_helper.rb
+++ lib/formtastic-bootstrap/helpers/errors_helper.rb
@@ -59,7 +59,7 @@ module FormtasticBootstrap
         end
 
         template.content_tag(:div, html_options) do
-          template.content_tag(:button, "&times;".html_safe, :class => "close", "data-dismiss" => "alert") +
+          template.content_tag(:a, "&times;".html_safe, :class => "close", "data-dismiss" => "alert") +
           template.content_tag(:ul, {:class => "error-list"}) do
             Formtastic::Util.html_safe(full_errors.map { |error| template.content_tag(:li, Formtastic::Util.html_safe(error)) }.join)
           end
